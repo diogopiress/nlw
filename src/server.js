119904576b90347c96ee17ed6420285f3d8557ed
diogopiress <67446406+diogopiress@users.noreply.github.com>
2020-08-17 20:24:52 -0300

    ate onde deu!
    ate onde deu, pois acabou o prazo de fica no ar

--- src/server.js
+++ src/server.js
@@ -1,6 +1,58 @@
-require('express')()
-.get("/", (req,res) => {
-    return res.send("hi from NLW")
+const proffys = [
+    {
+        name:"Diego Fernandes",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "898899776655",
+        bio:"Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject:"Química",
+        const:"20" ,
+        weekday: [0],
+        time_from: [720],
+        time_to: [1220]
+    },
 
+    {
+        name:"Daniela Evangelista",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "898899776655",
+        bio:"Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject:"Química",
+        const:"20" ,
+        weekday: [0],
+        time_from: [720],
+        time_to: [1220]
+    }
+]
+
+function pageLanding(req, res) {
+    return res.render("index.html")
+}
+
+function pageStudy(req, res) {
+    return res.render("study.html", { proffys })
+}
+
+function pageGiveclasses(req, res) {
+    return res.render("give-classes.html")
+}
+
+const express = require('express')
+const server = express()
+
+
+//configurar nunjucks
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true, 
 })
+
+
+server
+// configurar arquivos estaticos(css, scripts, imagem)
+.use(express.static("public"))
+//rotas de aplicação
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveclasses)
 .listen(5500)
